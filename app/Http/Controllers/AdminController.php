c8260bf0bda34b077622e321fbda1beb8561a45a
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-09-14 20:12:20 +0200

    auth login Guest updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\RedirectResponse;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
-        return view('admin.admin_dashboard');
+        return view('admin.index');
     }// End Method
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }// End Method
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    }// End Admin
 }
