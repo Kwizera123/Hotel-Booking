932f30efd8da2e04a8283e073c9e2f0c3ea42036
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-10-21 22:00:16 +0200

    auth admin password change updated

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 
 class AdminController extends Controller
 {
@@ -66,5 +68,36 @@ class AdminController extends Controller
         $id = Auth::user()->id;
         $profileData = User::find($id);
         return view('admin.admin_change_password',compact('profileData'));
+    }// End Method
+
+    public function AdminPasswordUpdate(Request $request){
+
+        //validation
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if(!Hash::check($request->old_password, auth::user()->password)){
+
+            $notification = array(
+                'message' => 'Old Password does not match!',
+                'alert-type' => 'error'
+            );
+    
+            return redirect()->back()->with($notification);
+
+        }
+        // update the new password
+        User::whereId(auth::user()->id)->update([
+            'password' => Hash::make($request->new_password)
+        ]);
+
+        $notification = array(
+            'message' => 'Password Changed Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
     }// End Method
 }
