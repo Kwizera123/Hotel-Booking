d9ed10fd4c6cf1493f8686d9f3db5afba3440259
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-10-28 10:42:36 +0200

    User Profile Page updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function Index(){
         return view('frontend.index');
+    }// End Method
+
+    public function UserProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('frontend.dashboard.edit_profile',compact('profileData'));
+
     }// End Method
 }
