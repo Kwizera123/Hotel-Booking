f2202720ddc30c4cd0d7bc97f0a72a453d0a0724
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-11-14 19:08:31 +0200

    Team updating data page updated

--- app/Http/Controllers/Backend/TeamController.php
+++ app/Http/Controllers/Backend/TeamController.php
@@ -11,6 +11,7 @@ use Carbon\Carbon;
 class TeamController extends Controller
 {
     public function AllTeam(){
+
         $team = Team::latest()->get();
         return view('backend.team.all_team',compact('team'));
     }// End of Method
@@ -23,7 +24,7 @@ class TeamController extends Controller
         $image = $request->file('image');
         $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
         Image::make($image)->resize(550,670)->save('upload/team/'.$name_gen);
-        $save_url = 'upload/team'.$name_gen;
+        $save_url = 'upload/team/'.$name_gen;
 
         Team::insert([
             'name' => $request->name,
@@ -43,4 +44,59 @@ class TeamController extends Controller
 
         return redirect()->route('all.team')->with($notification);
     }// End of Method
+
+    public function EditTeam($id){
+
+        $item = Team::findOrFail($id);
+        return view('backend.team.edit_team',compact('item'));
+    }// End Method
+
+    public function UpdateTeam(Request $request){
+        $team_id = $request->id;
+
+        if($request->file('image')){
+
+            $image = $request->file('image');
+            $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+            Image::make($image)->resize(550,670)->save('upload/team/'.$name_gen);
+            $save_url = 'upload/team/'.$name_gen;
+    
+            Team::findOrFail($team_id)->update([
+                'name' => $request->name,
+                'position' => $request->position,
+                'facebook' => $request->facebook,
+                'image' => $save_url,
+                'tweeter' => $request->tweeter,
+                'instagram' => $request->instagram,
+                'pinterest' => $request->pinterest,
+                'created_at' => Carbon::now(),
+            ]);
+    
+            $notification = array(
+                'message' => 'Team Update with Image Successfully',
+                'alert-type' => 'success'
+            );
+    
+            return redirect()->route('all.team')->with($notification);
+        }else{
+
+            Team::findOrFail($team_id)->update([
+                'name' => $request->name,
+                'position' => $request->position,
+                'facebook' => $request->facebook,
+                'tweeter' => $request->tweeter,
+                'instagram' => $request->instagram,
+                'pinterest' => $request->pinterest,
+                'created_at' => Carbon::now(),
+            ]);
+    
+            $notification = array(
+                'message' => 'Team Update without Image Successfully',
+                'alert-type' => 'info'
+            );
+    
+            return redirect()->route('all.team')->with($notification);
+
+        }// if condition ends
+    }// End Method
 }
