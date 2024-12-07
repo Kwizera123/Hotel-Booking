b7bc96f4a9f446ddfe7e8d2897861316a9c03c27
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-12-07 22:58:16 +0200

    Update Room Part 1 in Project

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Room;
 use App\Models\Facility;
 use Intervention\Image\Facades\Image;
+
 use Carbon\Carbon;
 
 
@@ -16,5 +17,32 @@ class RoomController extends Controller
         $basic_facility = Facility::where('rooms_id',$id)->get();
         $editData = Room::find($id);
         return view('backend.allroom.rooms.edit_rooms',compact('editData','basic_facility'));
+    }// End Method
+
+    public function UpdateRoom(Request $request, $id){
+        $room = Room::find($id);
+        $room->roomtype_id = $room->roomtype_id;
+        $room->total_child = $request->total_child;
+        $room->room_capacity = $request->room_capacity;
+        $room->price = $request->price;
+
+        $room->size = $request->size;
+        $room->view = $request->view;
+        $room->beg_style = $request->beg_style;
+        $room->discount = $request->discount;
+        $room->short_desc = $request->short_desc;
+        $room->description = $request->description;
+        /// Updat single Image
+        if($request->file('image')){
+        $image = $request->file('image');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(550,850)->save('upload/roomimg/'.$name_gen);
+        $room['image'] = $name_gen;
+        }
+
+        $room->save();
+
+
+
     }// End Method
 }
