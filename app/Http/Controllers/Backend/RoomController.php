92c9f89e80d2461b4c4928215a701dfaa4f9b2f7
Kwizera Theo <kwizerasinapy49@gmail.com>
2024-12-08 20:30:14 +0200

    Update Rooms Part 2 in Project

--- app/Http/Controllers/Backend/RoomController.php
+++ app/Http/Controllers/Backend/RoomController.php
@@ -42,7 +42,25 @@ class RoomController extends Controller
 
         $room->save();
 
+        ///Update for facility table
 
+        if($request->facility_name == NULL){
+            $notification = array(
+                'message' => 'Sorry! Not Any Facility Salacted',
+                'alert-type' => 'error'
+            );
+    
+            return redirect()->back()->with($notification);
+        }else{
+            Facility::where('rooms_id',$id)->delete();
+            $facilities = Count($request->facility_name);
+            for($i=0; $i < $facilities; $i++){
+                $fcount = new Facility();
+                $fcount->rooms_id = $room->id;
+                $fcount->facility_name = $request->facility_name[$i];
+                $fcount->save();
+            }// end for
+        }// end else
 
     }// End Method
 }
